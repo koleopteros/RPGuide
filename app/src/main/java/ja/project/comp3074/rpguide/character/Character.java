0d6518d5d0e02cdfdb288b7c79af46c8ab822f54
slidyklops <slidyklops@gmail.com>
2018-12-04 21:35:30 -0500

    just some setting up

---
+++ app/src/main/java/ja/project/comp3074/rpguide/character/Character.java
@@ -0,0 +1,12 @@
+package ja.project.comp3074.rpguide.character;
+
+public class Character {
+    private String name;
+    private int str,dex,intel,wis,cha,luk;
+    public Character(){
+
+    }
+    public Character(int[] values){
+
+    }
+}
